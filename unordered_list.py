c4ef9db9b265694fb3aea0c506d8008125d9d60e
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-07-06 01:50:51 +0900

    add add remove method

--- unordered_list.py
+++ unordered_list.py
@@ -16,6 +16,37 @@ class UnorderedList:
         self.head = None
     def isEmpty(self):
         return self.head == None
-
-temp = Node(93)
-print(temp.getData())
+    def add(self, item):
+        temp = Node(item)
+        temp.next = self.head
+        self.head = temp
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
